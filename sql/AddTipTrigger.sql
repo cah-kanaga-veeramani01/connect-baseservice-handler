8c4d843f236fdd7af867b4af1058d635ae6d7d6a
Snehal Jain <76516404+cah-snehal-jain@users.noreply.github.com>
2022-04-29 16:58:13 +0530

    [HL-723]: Initial commit for TIP related triggers

---
+++ sql/AddTipTrigger.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION ADD_TIP() RETURNS TRIGGER AS $$
+DECLARE thisisPK INTEGER;
+DECLARE userID INTEGER;
+DECLARE serviceTagID INTEGER;
+DECLARE startDate TIMESTAMP;
+DECLARE endDate TIMESTAMP := NULL;
+-- DECLARE expirationDays INTEGER;
+-- DECLARE expirationType VARCHAR;
+DECLARE serviceID INTEGER;
+BEGIN
+IF (TG_OP = 'INSERT') 
+THEN
+
+thisisPK = (select "TIPDetailRuleID" from attunityservice."TipDetailRule" where detailid = NEW."tipdetailid" ORDER BY "activeasof" DESC LIMIT 1);
+userID = (select "createUserID" from attunityservice."TIPDetailRuleOverview" where "TIPDetailRuleID" = thisisPK);
+serviceTagID = (select "TIPTypeID" from attunityservice."TipDetailRule" where "TIPDetailRuleID" = thisisPK);
+startDate = (select "activeasof" from attunityservice."TipDetailRule" where "TIPDetailRuleID" = thisisPK);
+endDate = (select "activethru" from attunityservice."TipDetailRule" where "TIPDetailRuleID" = thisisPK);
+-- expirationType = (select "expirationtype" from attunityservice."TIPDetailRule" where "TIPDetailRuleID" = thisisPK);
+-- expirationDays = (select "expirationdays" from attunityservice."TIPDetailRule" where "TIPDetailRuleID" = thisisPK);
+
+INSERT INTO service."Service"(
+ 	"serviceName", "serviceDisplayName", "globalServiceVersion", "validFrom", "validTill", "isPublished", "serviceTypeID", "createdAt", "createdBy", "legacyTipDetailID")
+ 	VALUES (NEW.title, NEW.title, 1, startDate, endDate, NEW.active, 1, NOW(), userID, NEW.tipdetailid) ON CONFLICT DO NOTHING RETURNING "serviceID" INTO serviceID;
+	
+INSERT INTO service."ServiceTagMapping"(
+	"serviceID", "globalServiceVersion", "serviceTagID", "createdAt", "createdBy")
+	VALUES (serviceID, 1, serviceTagID, NOW(), userID) ON CONFLICT DO NOTHING;
+
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
---
+++ sql/UpdateTipTrigger.sql
@@ -0,0 +1,49 @@
+CREATE OR REPLACE FUNCTION UPDATE_TIP() RETURNS TRIGGER AS $$
+DECLARE tipName VARCHAR;
+DECLARE userID INTEGER;
+DECLARE serviceTagID INTEGER;
+DECLARE startDate TIMESTAMP;
+DECLARE endDate TIMESTAMP := NULL;
+-- DECLARE expirationDays INTEGER;
+-- DECLARE expirationType VARCHAR;
+DECLARE serviceID INTEGER;
+BEGIN
+IF(TG_OP = 'INSERT')
+THEN
+
+IF ((select COUNT(*) from attunityservice."TIPDetailRule" where tipdetailid = NEW."tipdetailid") > 1) THEN
+
+tipName = (select "tiptitle" from attunityservice."TipDetail" where tipdetailid = NEW."tipdetailid");
+userID = (select "createUserID" from attunityservice."TIPDetailRuleOverview" where "TIPDetailRuleID" = NEW."TIPDetailRuleID");
+serviceTagID = NEW."TIPTypeID";
+startDate = NEW."activeasof";
+endDate = NEW."activethru";
+
+UPDATE
+   service."Service"
+SET
+   "serviceName" = tipName,
+   "serviceDisplayName" = tipName,
+   "validFrom" = startDate,
+   "validTill" = endDate,
+   "isPublished" = NEW.active,
+   "updatedAt" = NOW(),
+   "updatedBy" = userID
+WHERE
+    ( "legacyTipDetailID" = NEW."tipdetailid" ) RETURNING "serviceID" INTO serviceID;
+
+UPDATE
+    service."ServiceTagMapping"
+SET
+   "serviceTagID"= serviceTagID,
+   "updatedAt" = NOW(),
+   "updatedBy" = userID
+WHERE
+    ( "serviceID" = serviceID );
+
+END IF;
+
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
