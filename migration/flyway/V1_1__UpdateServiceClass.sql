9e4d6c38a44eed45494b67648a43be9edd2c587e
cah-kanaga-veeramani01 <kanaga.veeramani01@cardinalhealth.com>
2022-05-02 18:38:13 +0530

    Commented old migration scripts

--- migration/flyway/V1_1__UpdateServiceClass.sql
+++ migration/flyway/V1_1__UpdateServiceClass.sql
@@ -1,23 +1,23 @@
-ALTER TABLE IF EXISTS service."ServiceClass"
-RENAME TO "ServiceTag";
+-- ALTER TABLE IF EXISTS service."ServiceClass"
+-- RENAME TO "ServiceTag";
 
-ALTER TABLE service."ServiceTag"
-DROP COLUMN "serviceTypeID";
+-- ALTER TABLE service."ServiceTag"
+-- DROP COLUMN IF EXISTS "serviceTypeID";
 
-ALTER TABLE service."ServiceTag"
-RENAME COLUMN "serviceClassID" TO "serviceTagID";
+-- ALTER TABLE service."ServiceTag"
+-- RENAME COLUMN "serviceClassID" TO "serviceTagID";
 
-ALTER TABLE service."ServiceTag"
-RENAME COLUMN "serviceClassName" TO "serviceTagName";
+-- ALTER TABLE service."ServiceTag"
+-- RENAME COLUMN "serviceClassName" TO "serviceTagName";
 
-ALTER TABLE IF EXISTS service."ServiceClassHistories"
-RENAME TO "ServiceTagHistories";
+-- ALTER TABLE IF EXISTS service."ServiceClassHistories"
+-- RENAME TO "ServiceTagHistories";
 
-ALTER TABLE service."ServiceTagHistories"
-DROP COLUMN "serviceTypeID";
+-- ALTER TABLE service."ServiceTagHistories"
+-- DROP COLUMN "serviceTypeID";
 
-ALTER TABLE service."ServiceTagHistories"
-RENAME COLUMN "serviceClassID" TO "serviceTagID";
+-- ALTER TABLE service."ServiceTagHistories"
+-- RENAME COLUMN "serviceClassID" TO "serviceTagID";
 
-ALTER TABLE service."ServiceTagHistories"
-RENAME COLUMN "serviceClassName" TO "serviceTagName";
+-- ALTER TABLE service."ServiceTagHistories"
+-- RENAME COLUMN "serviceClassName" TO "serviceTagName";
