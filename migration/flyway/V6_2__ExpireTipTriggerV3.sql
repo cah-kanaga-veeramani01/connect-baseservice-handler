61f5d72f047c662656d53d81a38f3d092fc03965
Snehal Jain <76516404+cah-snehal-jain@users.noreply.github.com>
2022-05-16 18:29:52 +0530

    Added trigger for expiration scenario

---
+++ migration/flyway/V6_2__ExpireTipTriggerV3.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE FUNCTION EXPIRE_TIP() RETURNS TRIGGER AS $$
+DECLARE endDate TIMESTAMP := NULL;
+DECLARE tipStatus INTEGER = 0;
+BEGIN
+IF(TG_OP = 'UPDATE')
+THEN
+
+IF (NEW."active" = true)
+THEN
+tipStatus = 1;
+END IF;
+endDate = NEW."activethru";
+
+UPDATE
+   service."Service"
+SET
+   "validTill" = endDate,
+   "isPublished" = tipStatus,
+   "updatedAt" = NOW()
+WHERE
+    ( "legacyTIPDetailID" = NEW."tipdetailid" );
+
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+DROP TRIGGER IF EXISTS EXPIRE_TIP
+ON "attunityservice"."TipDetailRule";
+
+CREATE TRIGGER EXPIRE_TIP 
+AFTER UPDATE ON "attunityservice"."TipDetailRule"
+FOR EACH ROW
+EXECUTE PROCEDURE EXPIRE_TIP();
