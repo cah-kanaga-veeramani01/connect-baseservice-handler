c2f05f74ad98a417cad3002dd740dd7d1e84d42b
cah-harshith-kk <harshith.kk@cardinalhealth.com>
2022-10-17 10:40:37 +0530

    updated service type

--- migration/flyway/v8_1__FeedAttributesDefinitionAndMasterData.sql
+++ migration/flyway/v8_1__FeedAttributesDefinitionAndMasterData.sql
@@ -2,5 +2,5 @@ INSERT INTO service."AttributesDefinition"("attributesDefinitionID", name, "cate
 INSERT INTO service."AttributesDefinition"("attributesDefinitionID", name, "categoryName") VALUES(2,'COST','MEASURE') ON CONFLICT DO NOTHING;
 INSERT INTO service."AttributesDefinition"("attributesDefinitionID", name, "categoryName") VALUES(3,'QUALITY','MEASURE') ON CONFLICT DO NOTHING;
 
-INSERT INTO service."AttributesMaster"("attributesMasterID", "marketType", metadata, "createdAt") VALUES (1, 'TIP', '{"attributes":[1,2,3]}', now()) ON CONFLICT DO NOTHING;
+INSERT INTO service."AttributesMaster"("attributesMasterID", "serviceType", metadata, "createdAt") VALUES (1, 'TIP', '{"attributes":[1,2,3]}', now()) ON CONFLICT DO NOTHING;
 
