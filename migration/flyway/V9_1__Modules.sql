c2d75c7de3c790a13dd1de5e586de2d283115f10
cah-harshith-kk <harshith.kk@cardinalhealth.com>
2022-11-07 16:59:07 +0530

    HL-1004:Created moduleConfig tables for services

---
+++ migration/flyway/V9_1__Modules.sql
@@ -0,0 +1,25 @@
+-- Table: service.Modules
+
+-- DROP TABLE service."Modules";
+
+CREATE SEQUENCE IF NOT EXISTS service."Modules_moduleID_seq";
+
+CREATE TABLE IF NOT EXISTS service."Modules"
+(
+    "moduleID" integer NOT NULL DEFAULT nextval('service."Modules_moduleID_seq"'::regclass),
+    "moduleName" character(100) COLLATE pg_catalog."default" NOT NULL,
+    "isMandatory" boolean DEFAULT false,
+    CONSTRAINT "Modules_pkey" PRIMARY KEY ("moduleID")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE service."Modules"
+    OWNER to pcdb_admin;
+
+GRANT ALL ON TABLE service."Modules" TO pcdb_admin;
+
+GRANT DELETE, UPDATE, INSERT, SELECT ON TABLE service."Modules" TO pcdb_dml;
+
+INSERT INTO service."Modules"( "moduleName", "isMandatory")
+    VALUES ( 'Base Service Info', true);
