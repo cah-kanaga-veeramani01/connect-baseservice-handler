6ff42bd4a0964e3ef95f360ac897ea89c71168ed
cah-rajeshkumar-pradhan <rajeshkumar.pradhan@cardinalhealth.com>
2022-12-21 11:10:27 +0530

    HL-994 adding cookie check

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import httpContext from 'express-http-context';
-import { HandleError, invoke, HTTP_STATUS_CODES } from '../../utils';
+import { HandleError, invoke, HTTP_STATUS_CODES, logger } from '../../utils';
 
 const authUrl = `${process.env.AUTH_URL}${process.env.AUTH_ROUTE}`;
 
@@ -13,11 +13,16 @@ const authUrl = `${process.env.AUTH_URL}${process.env.AUTH_ROUTE}`;
  */
 export const auth = async (req: Request, _res: Response, next: NextFunction) => {
 	try {
+		logger.nonPhi.debug('cookies', req.headers.cookie);
+		if (!req.headers.cookie) {
+			throw new HandleError({ name: 'AuthenticationError', errorStatus: HTTP_STATUS_CODES.unauthenticated, message: 'Error while authenticating', stack: 'Error while authenticating' });
+		}
 		const authRes = await invoke({
 			method: 'GET',
 			url: authUrl,
 			headers: { cookie: req.headers.cookie }
 		});
+
 		if (authRes.status === HTTP_STATUS_CODES.ok) {
 			httpContext.set('userId', authRes.data.userId);
 			httpContext.set('userRoles', authRes.data.userRoles);
@@ -25,7 +30,7 @@ export const auth = async (req: Request, _res: Response, next: NextFunction) => 
 		} else {
 			throw new HandleError({ name: 'AuthenticationError', errorStatus: HTTP_STATUS_CODES.unauthenticated, message: 'Error while authenticating', stack: 'Error while authenticating' });
 		}
-	} catch (error) {
+	} catch (error: any) {
 		if (error instanceof HandleError) next(error);
 		else next(new HandleError({ name: 'AuthenticationError', errorStatus: HTTP_STATUS_CODES.unauthenticated, message: 'Error while authenticating', stack: 'Error while authenticating' }));
 	}
