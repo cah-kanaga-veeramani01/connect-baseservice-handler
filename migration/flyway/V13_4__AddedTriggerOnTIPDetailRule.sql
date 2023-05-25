224dbef295f4b4da76881d3a045f497035b6fe10
Snehal Jain <76516404+cah-snehal-jain@users.noreply.github.com>
2023-05-25 13:51:35 +0530

    Modified trigger functions

--- migration/flyway/V13_4__AddedTriggerOnTIPDetailRule.sql
+++ migration/flyway/V13_4__AddedTriggerOnTIPDetailRule.sql
@@ -1,57 +1,49 @@
 CREATE OR REPLACE FUNCTION SERVICE_ATTRIBUTES_TRIGGER() RETURNS TRIGGER AS $$
 DECLARE tipName VARCHAR;
-DECLARE userID INTEGER;
 DECLARE startDate TIMESTAMP;
 DECLARE endDate TIMESTAMP := NULL;
 DECLARE serviceID INTEGER;
 DECLARE tipStatus INTEGER = 0;
 DECLARE serviceTypeID INTEGER;
-DECLARE tipID INTEGER;
 DECLARE attributesDt JSON;
 DECLARE globalServiceVersion INTEGER;
-BEGIN IF(TG_OP = 'INSERT') THEN tipID = (
-   SELECT "tipdetailid"
-   from attunityservice."TIPDetailRule"
-   where "TIPDetailRuleID" = NEW."TIPDetailRuleID"
-);
+BEGIN
 IF (
    (
-      select COUNT(*)
+      select COUNT("tipdetailid")
       from attunityservice."TIPDetailRule"
-      where "tipdetailid" = tipID
+      where "tipdetailid" = NEW."tipdetailid"
    ) = 1
 ) THEN -- insert
-startDate = (
-   select "activeasof"
-   from attunityservice."TIPDetailRule"
-   where "TIPDetailRuleID" = NEW."TIPDetailRuleID"
-);
-endDate = (
-   select "activethru"
-   from attunityservice."TIPDetailRule"
-   where "TIPDetailRuleID" = NEW."TIPDetailRuleID"
-);
+
 serviceTypeID = (
    SELECT "serviceTypeID"
    FROM service."ServiceType"
    WHERE "serviceType" = 'TIP'
 );
+
 attributesDt = (
    SELECT json_build_object('attributes', '[]'::JSON)
 );
-IF (
-(select "active" from attunityservice."TIPDetailRule" where "TIPDetailRuleID" = NEW."TIPDetailRuleID") = true
-) THEN tipStatus = 1;
+
+startDate = NEW."activeasof";
+
+endDate = NEW."activethru";
+
+IF ((NEW."active") = true)
+ THEN tipStatus = 1;
 END IF;
 
-IF (tipStatus = 0) THEN endDate = NOW() - INTERVAL '1 DAY'; END IF;
+IF (tipStatus = 0)
+ THEN endDate = NOW() - INTERVAL '1 DAY';
+END IF;
 
-userID = NEW."createUserID";
 tipName = (
    select "tiptitle"
    from attunityservice."TIPDetail"
-   where "tipdetailid" = tipID
+   where "tipdetailid" = NEW."tipdetailid"
 );
+
 INSERT INTO service."Service"(
       "serviceName",
       "serviceDisplayName",
@@ -61,7 +53,6 @@ INSERT INTO service."Service"(
       "isPublished",
       "serviceTypeID",
       "createdAt",
-      "createdBy",
       "legacyTIPDetailID"
    )
 VALUES (
@@ -73,69 +64,60 @@ VALUES (
       tipStatus,
       serviceTypeID,
       NOW(),
-      userID,
-      tipID
+      NEW."tipdetailid"
    ) ON CONFLICT DO NOTHING
 RETURNING "serviceID" INTO serviceID;
 INSERT INTO service."ServiceAttributes" (
       "metadata",
       "serviceID",
       "globalServiceVersion",
-      "createdAt",
-      "createdBy"
+      "createdAt"
    )
-VALUES (attributesDt, serviceID, 1, NOW(), userID) ON CONFLICT DO NOTHING;
+VALUES (attributesDt, serviceID, 1, NOW()) ON CONFLICT DO NOTHING;
 END IF;
 IF (
    (
-      select COUNT(*)
+      select COUNT("tipdetailid")
       from attunityservice."TIPDetailRule"
-      where "tipdetailid" = tipID
+      where "tipdetailid" = NEW."tipdetailid"
    ) > 1
 ) THEN -- update
-startDate = (
-   select "activeasof"
-   from attunityservice."TIPDetailRule"
-   where "TIPDetailRuleID" = NEW."TIPDetailRuleID"
-);
-endDate = (
-   select "activethru"
-   from attunityservice."TIPDetailRule"
-   where "TIPDetailRuleID" = NEW."TIPDetailRuleID"
-);
-IF (
-(select "active" from attunityservice."TIPDetailRule" where "TIPDetailRuleID" = NEW."TIPDetailRuleID") = true
-) THEN tipStatus = 1;
-END IF;
 
-IF (tipStatus = 0) THEN endDate = NOW() - INTERVAL '1 DAY'; END IF;
-
-userID = NEW."createUserID";
 attributesDt = (
    SELECT json_build_object('attributes', '[]'::JSON)
 );
 
+startDate = NEW."activeasof";
+
+endDate = NEW."activethru";
+
+IF ((NEW."active") = true)
+ THEN tipStatus = 1;
+END IF;
+
+IF (tipStatus = 0)
+ THEN endDate = NOW() - INTERVAL '1 DAY';
+END IF;
+
 tipName = (
    select "tiptitle"
    from attunityservice."TIPDetail"
-   where "tipdetailid" = tipID
+   where "tipdetailid" = NEW."tipdetailid"
 );
+
 UPDATE service."Service"
 SET "serviceName" = tipName,
    "serviceDisplayName" = tipName,
    "validFrom" = startDate,
    "validTill" = endDate,
    "isPublished" = tipStatus,
-   "updatedAt" = NOW(),
-   "updatedBy" = userID
-WHERE ("legacyTIPDetailID" = tipID)
+   "updatedAt" = NOW()
+WHERE ("legacyTIPDetailID" = NEW."tipdetailid")
 RETURNING "serviceID" INTO serviceID;
 UPDATE service."ServiceAttributes"
 SET "metadata" = attributesDt,
-   "updatedAt" = NOW(),
-   "updatedBy" = userID
+   "updatedAt" = NOW()
 WHERE ("serviceID" = serviceID);
-END IF;
 END IF;
 RETURN NEW;
 END;
@@ -143,4 +125,4 @@ $$ LANGUAGE 'plpgsql';
 DROP TRIGGER IF EXISTS SERVICE_ATTRIBUTES_TRIGGER ON "attunityservice"."TIPDetailRuleOverview";
 CREATE TRIGGER SERVICE_ATTRIBUTES_TRIGGER
 AFTER
-INSERT ON "attunityservice"."TIPDetailRuleOverview" FOR EACH ROW EXECUTE PROCEDURE SERVICE_ATTRIBUTES_TRIGGER();
+INSERT ON "attunityservice"."TIPDetailRule" FOR EACH ROW EXECUTE PROCEDURE SERVICE_ATTRIBUTES_TRIGGER();
---
+++ migration/flyway/V13_5__UpdatedTriggerOnRuleOverview.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION SERVICE_USERID_TRIGGER() RETURNS TRIGGER AS $$
+DECLARE userID INTEGER;
+DECLARE tipID INTEGER;
+DECLARE serviceID INTEGER;
+BEGIN
+tipID = (
+   SELECT "tipdetailid"
+   from attunityservice."TIPDetailRule"
+   where "TIPDetailRuleID" = NEW."TIPDetailRuleID"
+);
+
+UPDATE service."Service"
+SET "updatedBy" = userID
+WHERE ("legacyTIPDetailID" = tipID)
+RETURNING "serviceID" INTO serviceID;
+UPDATE service."ServiceAttributes"
+SET "updatedBy" = userID
+WHERE ("serviceID" = serviceID);
+
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+DROP TRIGGER IF EXISTS SERVICE_ATTRIBUTES_TRIGGER ON "attunityservice"."TIPDetailRuleOverview";
+CREATE TRIGGER SERVICE_USERID_TRIGGER
+AFTER
+INSERT ON "attunityservice"."TIPDetailRuleOverview" FOR EACH ROW EXECUTE PROCEDURE SERVICE_USERID_TRIGGER();
