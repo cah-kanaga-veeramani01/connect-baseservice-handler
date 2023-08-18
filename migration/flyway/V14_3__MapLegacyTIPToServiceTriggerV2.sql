7d4f6b555b6ae15ef0241a88cdb58c18041387b1
rajeshkumar.pradhan <rajeshkumar.pradhan@cardinalhealth.com>
2023-08-18 17:03:46 +0530

    HL-1549 corrected tip syncing

---
+++ migration/flyway/V14_3__MapLegacyTIPToServiceTriggerV2.sql
@@ -0,0 +1,163 @@
+CREATE OR REPLACE FUNCTION MAP_LEGACY_TIP_TO_SERVICE() RETURNS TRIGGER AS $$
+DECLARE tipName VARCHAR;
+DECLARE startDate TIMESTAMP;
+DECLARE endDate TIMESTAMP := NULL;
+DECLARE serviceID INTEGER;
+DECLARE tipStatus INTEGER = 0;
+DECLARE serviceTypeID INTEGER;
+DECLARE attributesDt JSON;
+DECLARE globalServiceVersion INTEGER;
+BEGIN
+
+IF (
+   (
+      select COUNT("tipdetailid")
+      from attunityservice."TIPDetailRule"
+      where "tipdetailid" = NEW."tipdetailid"
+   ) = 1
+) THEN -- insert
+
+serviceTypeID = (
+   SELECT "serviceTypeID"
+   FROM service."ServiceType"
+   WHERE "serviceType" = 'TIP'
+);
+
+attributesDt = (
+   SELECT json_build_object('attributes', '[]'::JSON)
+);
+
+startDate = NEW."activeasof";
+
+endDate = NEW."activethru";
+
+IF ((NEW."active") = true)
+ THEN tipStatus = 1;
+END IF;
+
+IF (tipStatus = 0)
+ THEN endDate = NOW() - INTERVAL '1 DAY';
+END IF;
+
+tipName = (
+   select "tiptitle"
+   from attunityservice."TIPDetail"
+   where "tipdetailid" = NEW."tipdetailid"
+);
+
+INSERT INTO service."Service"(
+      "serviceName",
+      "serviceDisplayName",
+      "globalServiceVersion",
+      "validFrom",
+      "validTill",
+      "isPublished",
+      "serviceTypeID",
+      "createdAt",
+      "legacyTIPDetailID"
+   )
+VALUES (
+      tipName,
+      tipName,
+      1,
+      startDate,
+      endDate,
+      tipStatus,
+      serviceTypeID,
+      NOW(),
+      NEW."tipdetailid"
+   ) ON CONFLICT DO NOTHING
+RETURNING "serviceID" INTO serviceID;
+INSERT INTO service."ServiceAttributes" (
+      "metadata",
+      "serviceID",
+      "globalServiceVersion",
+      "createdAt"
+   )
+VALUES (attributesDt, serviceID, 1, NOW()) ON CONFLICT DO NOTHING;
+
+END IF;
+IF (
+   (
+      select COUNT("tipdetailid")
+      from attunityservice."TIPDetailRule"
+      where "tipdetailid" = NEW."tipdetailid"
+   ) > 1
+) THEN -- update
+
+startDate = NEW."activeasof";
+
+endDate = NEW."activethru";
+
+IF ((NEW."active") = true)
+ THEN tipStatus = 1;
+END IF;
+
+IF (tipStatus = 0)
+ THEN endDate = NOW() - INTERVAL '1 DAY';
+END IF;
+
+tipName = (
+   select "tiptitle"
+   from attunityservice."TIPDetail"
+   where "tipdetailid" = NEW."tipdetailid"
+);
+
+IF (
+   (
+      select COUNT("serviceID")
+      from service."Service"
+      where "legacyTIPDetailID" = NEW."tipdetailid" and "validFrom" is null and "validTill" is null AND "isPublished" = 0
+   ) = 1
+) THEN 
+-- draft
+
+UPDATE service."Service"
+SET 
+"serviceName" = tipName,
+   "serviceDisplayName" = tipName,
+   "validTill" = null,
+   "updatedAt" = NOW()
+WHERE "globalServiceVersion"=(select "globalServiceVersion" from service."Service" 
+							where "legacyTIPDetailID" = NEW."tipdetailid" order by "globalServiceVersion" desc limit 1 offset 1)
+AND "legacyTIPDetailID" = NEW."tipdetailid"
+RETURNING "serviceID" INTO serviceID;
+
+ELSIF (
+   (
+      select COUNT("serviceID")
+      from service."Service"
+      where "legacyTIPDetailID" = NEW."tipdetailid" and 
+	  ("validFrom" IS NOT NULL AND "validFrom" > now() AND (("validTill" IS NOT NULL AND "validFrom" < "validTill") OR ("validTill" IS NULL))) 
+  	AND "isPublished" = 1
+   ) = 1
+)
+THEN
+-- schedule
+
+
+ELSE
+-- active
+
+UPDATE service."Service"
+SET 
+"serviceName" = tipName,
+   "serviceDisplayName" = tipName,
+   "validTill" = null,
+   "updatedAt" = NOW()
+WHERE "globalServiceVersion"=(select "globalServiceVersion" from service."Service" 
+							where "legacyTIPDetailID" = NEW."tipdetailid" order by "globalServiceVersion" desc limit 1 offset 0)
+AND "legacyTIPDetailID" = NEW."tipdetailid"
+RETURNING "serviceID" INTO serviceID;
+
+
+END IF;
+
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+DROP TRIGGER IF EXISTS MAP_LEGACY_TIP_TO_SERVICE ON "attunityservice"."TIPDetailRule";
+CREATE TRIGGER MAP_LEGACY_TIP_TO_SERVICE
+AFTER
+INSERT ON "attunityservice"."TIPDetailRule" FOR EACH ROW EXECUTE PROCEDURE MAP_LEGACY_TIP_TO_SERVICE();
