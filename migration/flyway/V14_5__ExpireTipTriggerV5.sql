9ea1a074f59c114d3f8a9a6384480682fa457016
rajeshkumar.pradhan <rajeshkumar.pradhan@cardinalhealth.com>
2023-08-21 12:42:01 +0530

    HL-1549 remove update triggers

--- migration/flyway/V14_5__ExpireTipTriggerV5.sql
+++ migration/flyway/V14_5__ExpireTipTriggerV5.sql
@@ -1,40 +1,5 @@
-CREATE OR REPLACE FUNCTION EXPIRE_TIP() RETURNS TRIGGER AS $$
-DECLARE endDate TIMESTAMP := NULL;
-DECLARE tipStatus INTEGER = 0;
-BEGIN
-
-IF(TG_OP = 'UPDATE')
-THEN
-
-IF (NEW."active" = false)
-THEN
-endDate = NEW."activethru";
-
-UPDATE
-   service."Service"
-SET
-   "validTill" = endDate,
-   "updatedAt" = NOW()
-WHERE
-    ( "legacyTIPDetailID" = NEW."tipdetailid" AND 
-	(("validFrom" < now() AND "validTill" >= now()) OR ("validFrom" < now() AND "validTill" IS NULL)) AND "isPublished" = 1
-	);
-
-
-END IF;
-
-END IF;
-RETURN NEW;
-END;
-$$ LANGUAGE 'plpgsql';
-
 DROP TRIGGER IF EXISTS EXPIRE_TIP
 ON "attunityservice"."TIPDetailRule";
-
-CREATE TRIGGER EXPIRE_TIP 
-AFTER UPDATE ON "attunityservice"."TIPDetailRule"
-FOR EACH ROW
-EXECUTE PROCEDURE EXPIRE_TIP();
 
 
 
