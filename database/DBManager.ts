4b2c1c18b6823059c8bfc1dfd126b79771f266fb
rajeshkumar.pradhan <rajeshkumar.pradhan@cardinalhealth.com>
2023-11-09 18:59:00 +0530

    update

--- database/DBManager.ts
+++ database/DBManager.ts
@@ -18,8 +18,12 @@ try {
 		},
 		dialect: 'postgres',
 		dialectOptions: {
-			ssl: String(process.env.DB_SSL).toLowerCase() === 'true',
-			trustServerCertificate: true
+			//ssl: String(process.env.DB_SSL).toLowerCase() === 'true',
+			//trustServerCertificate: true
+			ssl: {
+				require: true,
+				rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+			  }
 		},
 		username: process.env.DB_USERNAME_DDL,
 		password: process.env.DB_PASSWORD_DDL,
@@ -39,8 +43,12 @@ try {
 		},
 		dialect: 'postgres',
 		dialectOptions: {
-			ssl: String(process.env.DB_SSL).toLowerCase() === 'true',
-			trustServerCertificate: true,
+			//ssl: String(process.env.DB_SSL).toLowerCase() === 'true',
+			//trustServerCertificate: true,
+			ssl: {
+				require: true,
+				rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+			  }
 		},
 		username: process.env.DB_USERNAME_DML,
 		password: process.env.DB_PASSWORD_DML,
