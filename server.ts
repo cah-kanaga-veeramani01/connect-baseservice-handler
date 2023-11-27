c6c78fe98365ea17c603b6f9aa9f67175c8f6603
rajeshkumar.pradhan <rajeshkumar.pradhan@cardinalhealth.com>
2023-11-27 22:21:53 +0530

    update

--- server.ts
+++ server.ts
@@ -14,6 +14,7 @@ import session from 'express-session';
 import swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from './swagger.json';
 import * as swaggerExternalDocument from './swagger-external.json';
+import actuator from 'express-actuator';
 
 const memoryStore = new session.MemoryStore(),
 	keycloak = initKeyclock(memoryStore),
@@ -49,7 +50,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 
 app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(actuator());
 //middlewares for each request
 app.use(cookieParser());
 app.use(requestLogger);
@@ -57,6 +58,9 @@ app.use(express.json());
 app.use(httpContext.middleware);
 app.use(contextStore);
 app.use(generateLogId);
+app.use('/actuator/health', (req: Request, res: Response, _next: NextFunction) => {
+	res.json({ status: 'UP' });
+});
 app.use(/^((?!external|swagger).)*$/i, auth); // authenticate every route except /swagger
 
 app.get('/', (req: Request, res: Response, _next: NextFunction) => {
