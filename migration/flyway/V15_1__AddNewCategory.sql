2cf1e695f08b8c0b06cf3a98b577027c5ed22fe2
cah-kanaga-veeramani01 <kanaga.veeramani01@cardinalhealth.com>
2024-01-01 16:38:48 +0530

    HL-1684 - added new service config attribute

---
+++ migration/flyway/V15_1__AddNewCategory.sql
@@ -0,0 +1,3 @@
+INSERT INTO service."AttributesDefinition"(
+	 name, description, "createdAt", "updatedAt", "categoryName")
+	VALUES ('Quick TIP', 'TIPs that run a short workflow', now(), now(), 'Workflow Type');
