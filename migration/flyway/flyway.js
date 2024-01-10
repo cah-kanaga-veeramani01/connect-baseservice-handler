fee25a25342c53fa191e763a4b34e0c8077ee058
rajeshkumar.pradhan <rajeshkumar.pradhan@cardinalhealth.com>
2024-01-10 14:46:19 +0530

    updated

--- migration/flyway/flyway.js
+++ migration/flyway/flyway.js
@@ -2,19 +2,14 @@ module.exports = function () {
 	return {
 		flywayArgs: {
 			
-			url: `jdbc:postgresql://${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
+			url: `jdbc:postgresql://${process.env.DB_HOST}:5432/${process.env.DB_NAME}?ssl=true`,
 			schemas: process.env.DB_SCHEMA,
 			locations: 'filesystem:migration/flyway',
 			user: process.env.DB_USERNAME_DDL,
 			password: process.env.DB_PASSWORD_DDL,
 			sqlMigrationSuffixes: '.sql',
 			baselineOnMigrate: true,
-			"ssl":true,
-			"dialectOptions":{
-				"ssl":{
-					"require":true
-				}
-			}
+			
 		}
 	};
 };
