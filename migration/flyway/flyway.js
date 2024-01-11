6b523a671b2b8d89c279a23980d802af6fe1fd12
rajeshkumar.pradhan <rajeshkumar.pradhan@cardinalhealth.com>
2024-01-11 11:20:23 +0530

    updated

--- migration/flyway/flyway.js
+++ migration/flyway/flyway.js
@@ -7,7 +7,7 @@ module.exports = function () {
 	return {
 		flywayArgs: {
 			
-			url: `jdbc:postgresql://cc-client-operations-program-configuration-dev.cluster-cukimq938igp.us-east-1.rds.amazonaws.com:5432/postgres?ssl=true&sslmode=verify-ca&sslrootcert=./cert/us-east-1-bundle.pem`,
+			url: `jdbc:postgresql://cc-client-operations-program-configuration-dev.cluster-cukimq938igp.us-east-1.rds.amazonaws.com:5432/postgres?ssl=true&sslmode=require&sslrootcert=./cert/us-east-1-bundle.pem`,
 			"jdbcProperties.sslrootcert":"./../../cert/us-east-1-bundle.pem",
 			"jdbcProperties.sslcert":"./../../cert/us-east-1-bundle.crt",
 			schemas: process.env.DB_SCHEMA,
