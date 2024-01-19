7423c17aea693799f0ae4cfbc80de9623ed8389e
cah-nandita-sahu <89122591+cah-nandita-sahu@users.noreply.github.com>
2024-01-19 13:02:07 +0530

    HL-1712:Formatted

--- migration/flyway/V14_7__AddIsDiscardred.sql
+++ migration/flyway/V14_7__AddIsDiscardred.sql
@@ -1,20 +1,20 @@
 ALTER TABLE IF EXISTS service."Service"
     ADD COLUMN IF NOT EXISTS "isDiscarded" integer NOT NULL DEFAULT 0;
 
-CREATE OR replace PROCEDURE service.discard_service_draft(serviceids INT[])
+CREATE OR REPLACE PROCEDURE service.discard_service_draft(ServiceIDs INT[])
 AS
   $$
   DECLARE
   BEGIN
-    RAISE notice 'Value: %',cardinality(serviceids);
+    RAISE notice 'Value: %',cardinality(ServiceIDs);
     CASE
     WHEN (
-        cardinality(serviceids)>0
+        cardinality(ServiceIDs)>0
       )
       THEN
       UPDATE service."Service"
       SET    "isDiscarded" = 1
-      WHERE  "serviceID" = ANY(serviceids)
+      WHERE  "serviceID" = ANY(ServiceIDs)
       AND    "status" = 'DRAFT';
     
     ELSE
